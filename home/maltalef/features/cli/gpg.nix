0a105abe230900d213b01d81c4e367ef4b30ff69
Manuel Altalef <maltalef@auror.ar>
2023-08-21 10:55:18 -0300

    gpg

---
+++ home/maltalef/features/cli/gpg.nix
@@ -0,0 +1,44 @@
+{ pkgs, config, lib, ... }:
+let
+  pinentry =
+    if config.gtk.enable then {
+      packages = [ pkgs.pinentry-gnome pkgs.gcr ];
+      name = "gnome3";
+    } else {
+      packages = [ pkgs.pinentry-curses ];
+      name = "curses";
+    };
+in
+{
+  home.packages = pinentry.packages;
+
+  services.gpg-agent = {
+    enable = true;
+    #enableSshSupport = true;
+    pinentryFlavor = pinentry.name;
+    #enableExtraSocket = true;
+  };
+
+  programs.gpg = {
+    enable = true;
+    settings.trust-model = "tofu+pgp";
+  };
+
+#  systemd.user.services = {
+#    # Link /run/user/$UID/gnupg to ~/.gnupg-sockets
+#    # So that SSH config does not have to know the UID
+#    link-gnupg-sockets = {
+#      Unit = {
+#        Description = "link gnupg sockets from /run to /home";
+#      };
+#      Service = {
+#        Type = "oneshot";
+#        ExecStart = "${pkgs.coreutils}/bin/ln -Tfs /run/user/%U/gnupg %h/.gnupg-sockets";
+#        ExecStop = "${pkgs.coreutils}/bin/rm $HOME/.gnupg-sockets";
+#        RemainAfterExit = true;
+#      };
+#      Install.WantedBy = [ "default.target" ];
+#    };
+#  };
+}
+# vim: filetype=nix
