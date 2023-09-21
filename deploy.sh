8a905756f95073e6bb64a0285555800e11e7beb3
Manuel Altalef <maltalef@auror.ar>
2023-09-21 18:49:59 -0300

    build(deploy.sh): script for deploying on remote targets

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+export NIX_SSHOPTS="-t"
+
+build_remote=false
+
+hosts="$1"
+shift
+
+if [ -z "$hosts" ]; then
+    echo "No hosts to deploy"
+    exit 2
+fi
+
+for host in ${hosts//,/ }; do
+    nixos-rebuild --flake .\#$host switch --target-host $host --use-remote-sudo --use-substitutes $@
+done
+
