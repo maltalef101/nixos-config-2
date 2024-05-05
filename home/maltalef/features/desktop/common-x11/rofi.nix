15c4d1da7e8bb40d60b648ec96954cbf3064d043
Manuel Altalef <maltalef@auror.ar>
2024-05-05 13:20:59 -0300

    feat(home/rofi): sidebar mode and icons

--- home/maltalef/features/desktop/common-x11/rofi.nix
+++ home/maltalef/features/desktop/common-x11/rofi.nix
@@ -2,5 +2,11 @@
   programs.rofi = {
     enable = true;
     theme = "gruvbox-dark-hard.rasi";
+	terminal = "${pkgs.alacritty}/bin/alacritty";
+
+	extraConfig = {
+		show-icons = "true";
+		sidebar-mode = "true";
+	};
   };
 }
