c7d4331233ccae91ce8d7d1d9955af5820af1a80
Manuel Altalef <maltalef@auror.ar>
2024-10-25 04:22:59 -0300

    shellcheck

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,7 @@
 { pkgs ? import <nixpkgs> { } }: rec {
 	maimfull = pkgs.callPackage ./scripts/maimsel { };
 	maimsel = pkgs.callPackage ./scripts/maimfull { };
+
+	hyprshot-full = pkgs.callPackage ./scripts/hyprshot-full { };
+	hyprshot-region = pkgs.callPackage ./scripts/hyprshot-region { };
 }
