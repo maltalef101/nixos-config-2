16dba6320b5b953243c8aaa513013bec74cb920b
Manuel Altalef <maltalef@auror.ar>
2024-11-06 06:47:12 -0300

    tex packages

--- home/maltalef/features/tex/default.nix
+++ home/maltalef/features/tex/default.nix
@@ -3,6 +3,6 @@ let
   tex = (pkgs.texlive.combine {
     inherit (pkgs.texlive)
       biblatex scheme-medium wrapfig amsmath ulem hyperref capt-of dvisvgm
-      dvipng enumitem titlesec;
+      dvipng enumitem titlesec graphics;
   });
 in { home.packages = with pkgs; [ tex ]; }
