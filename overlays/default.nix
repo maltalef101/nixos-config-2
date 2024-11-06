f49270077c6f33257abd754908ead300d542ab6c
Manuel Altalef <maltalef@auror.ar>
2024-11-06 06:39:24 -0300

    hyprland overlay for libinput override

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,33 @@
 { inputs, outputs }: {
 	additions = final: _prev: import ../pkgs { pkgs = final; };
-	modifications = final: prev: { };
+
+	modifications = final: prev: {
+		hyprland-patched = let
+			libinput = prev.libinput.overrideAttrs (self: {
+				name = "libinput";
+				version = "1.26.0";
+				src = final.fetchFromGitLab {
+					domain = "gitlab.freedesktop.org";
+					owner = "libinput";
+					repo = "libinput";
+					rev = self.version;
+					hash = "sha256-mlxw4OUjaAdgRLFfPKMZDMOWosW9yKAkzDccwuLGCwQ=";
+				};
+			});
+		in
+			inputs.hyprland.packages.${prev.system}.hyprland.override {
+				libinput = libinput;
+				aquamarine = inputs.hyprland.inputs.aquamarine.packages.${prev.system}.aquamarine.override {
+					libinput = libinput;
+				};
+				wayland-scanner = final.unstable.wayland-scanner;
+			};
+	};
+
+	unstable-packages = final: _prev: {
+		unstable = import inputs.nixpkgs-unstable {
+			system = final.system;
+			config.allowUnfree = true;
+		};
+	};
 }
