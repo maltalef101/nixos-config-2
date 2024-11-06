7c602d56bc76b04b28a7218dbee5c74be43b75e2
Manuel Altalef <maltalef@auror.ar>
2024-11-06 06:44:04 -0300

    this was big

--- home/maltalef/vic20.nix
+++ home/maltalef/vic20.nix
@@ -1,7 +1,8 @@
 { inputs, outputs, lib, ... }: {
   imports = [
     ./global.nix
-    ./features/desktop/leftwm
+    #./features/desktop/leftwm
+    ./features/desktop/hyprland
     ./features/desktop/common/optional/piper.nix
     ./features/desktop/common/optional/network.nix
     ./features/emacs
