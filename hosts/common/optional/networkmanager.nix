5a44943e57944a7ead2a9488f3202f1ed3df4a51
Manuel Altalef <maltalef@auror.ar>
2025-03-04 07:19:22 -0300

    add ipsec strongswan to nm plugins

--- hosts/common/optional/networkmanager.nix
+++ hosts/common/optional/networkmanager.nix
@@ -1,4 +1,4 @@
 { pkgs, config, ...}: {
   networking.networkmanager.enable = true;
-  environment.systemPackages = [ pkgs.networkmanager-openvpn ];
+  environment.systemPackages = [ pkgs.networkmanager-openvpn pkgs.strongswanNM ];
 }
