23263d52663d3d0613dad02d77d50e7ee75c3290
me me <me@me.com>
2026-02-07 21:28:35 +0000

    stuff

---
+++ hosts/c128/default.nix
@@ -0,0 +1,33 @@
+{ pkgs, inputs, ... }: {
+  imports = with inputs.hardware.nixosModules; [
+    common-cpu-intel
+    common-gpu-intel
+    common-pc-ssd
+
+    ./hardware-configuration.nix
+
+    ../common
+    ../common/users/maltalef
+
+	../common/optional/auto-cpufreq.nix
+	../common/optional/brightness.nix
+	../common/optional/gamemode.nix
+	../common/optional/greetd.nix
+	../common/optional/thermald.nix
+	../common/optional/systemd-boot.nix
+    ../common/optional/bluetooth.nix
+    ../common/optional/networkmanager.nix
+    ../common/optional/pipewire.nix
+	../common/optional/hyprland.nix
+	../common/optional/work/undef.nix
+  ];
+
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+
+  programs.dconf.enable = true;
+
+  services.logind = {
+	  lidSwitchDocked = "ignore";
+	  lidSwitchExternalPower = "ignore";
+  };
+}
