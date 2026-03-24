c3781f4210da9b4f4cf45272594cc7a4ec3e8dbd
Manuel Altalef <maltalef@auror.ar>
2026-03-23 22:13:49 -0300

    mw

---
+++ home/mw/global.nix
@@ -0,0 +1,42 @@
+{ inputs, lib, pkgs, config, outputs, ...}:
+let
+  inherit (inputs.nix-colors) colorSchemes;
+in
+{
+  imports = [
+    inputs.nix-colors.homeManagerModule
+    ./features/cli
+  ];
+
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+
+  systemd.user = {
+    startServices = "sd-switch";
+	  sessionVariables = {
+      TERMINAL = "alacritty";
+	    PAGER = "less";
+      EDITOR = "nvim";
+      VISUAL = "emacs";
+      BROWSER = "firefox";
+	  };
+  };
+  
+  programs = {
+    home-manager.enable = true;
+    git.enable = true;
+  };
+
+  home = {
+    username = lib.mkDefault "mw";
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    stateVersion = lib.mkDefault "23.11";
+  };
+	
+	colorscheme = lib.mkDefault colorSchemes.gruvbox-dark-hard;
+  home.file.".colorscheme".text = config.colorscheme.slug;
+}
