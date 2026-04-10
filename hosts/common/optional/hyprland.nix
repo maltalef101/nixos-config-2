f8d2b8d1a75f054d94920688ca27d82fd3e8cae2
Manuel Altalef <maltalef@auror.ar>
2026-04-10 11:12:25 -0300

    pkgs: disable notify-battery-status package and configuration due to build issues

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -7,7 +7,7 @@
 
 	security.pam.services.hyprlock = {};
 
-	services.udev.extraRules = ''
-		SUBSYSTEM="power_supply", ENV{POWER_SUPPLY_TYPE}=="Battery", ATTR{status}=="Discharging", ATTR{capacity}=="[0-19]|20", RUN+="${pkgs.notify-battery-status}"
-	'';
+	# services.udev.extraRules = ''
+	# 	SUBSYSTEM="power_supply", ENV{POWER_SUPPLY_TYPE}=="Battery", ATTR{status}=="Discharging", ATTR{capacity}=="[0-19]|20", RUN+="${pkgs.notify-battery-status}"
+	# '';
 }
