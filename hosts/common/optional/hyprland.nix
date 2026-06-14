445c97abebc7cc3a78a72660847e8defce1e219d
Manuel Altalef <maltalef@auror.ar>
2026-06-14 10:01:50 -0300

    pkgs: remove notify-battery-status (replaced by batsignal)

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -6,8 +6,4 @@
 	};
 
 	security.pam.services.hyprlock = {};
-
-	# services.udev.extraRules = ''
-	# 	SUBSYSTEM="power_supply", ENV{POWER_SUPPLY_TYPE}=="Battery", ATTR{status}=="Discharging", ATTR{capacity}=="[0-19]|20", RUN+="${pkgs.notify-battery-status}"
-	# '';
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,5 +3,4 @@
 	maimsel = pkgs.callPackage ./scripts/maimfull { };
 	hyprshot-full = pkgs.callPackage ./scripts/hyprshot-full { };
 	hyprshot-region = pkgs.callPackage ./scripts/hyprshot-region { };
-	# notify-battery-status = pkgs.callPackage ./scripts/notify-battery-status { };
 }
